4b3aaa6887c51661fb5220c6cadec466087d332d
Felipe Martina <felipe.martina@3xmgroup.com>
2016-02-26 21:33:55 -0300

    Trying to remove monk.

--- mongo-dal.js
+++ mongo-dal.js
@@ -1,14 +1,21 @@
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/bitnode_test');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var ObjectID = require('mongodb').ObjectID;
 var logger = require('./logger');
+var config = require('./config');
 
-var ticks_collection = db.get('ticks');
+var db;
+module.exports = function () {
+    MongoClient.connect(config.mongo_server, function (err, connected_db) {
+        assert.equal(null, err);
+        db = connected_db;
+    });
+    ticks_dal: ticks_dal
+};
 var ticks_dal = {
     insert: function (tick, callback) {
         tick.timestamp = Date.now();
-        ticks_collection.insert(tick, function (err, doc) {
+        db.collection('ticks').insert(tick, function (err, doc) {
             if (err) {
                 logger.error('Cannot store current tick. err: %s tick:', err, tick);
             }
@@ -17,14 +24,14 @@ var ticks_dal = {
     },
 
     add_stat: function (tick_id, stat, callback) {
-        ticks_collection.update(tick_id, { $push: { stats: stat } }, function (err) {
+        db.collection('ticks').update(tick_id, { $push: { stats: stat } }, function (err) {
             if (err) {
                 logger.error('Cannot add stat for tick "%s". err: %s', tick_id, err);
             }
             if (callback) callback();
         });
-    }, 
-    
+    },
+
     read_last_ticks: function (timestamp, ticks_to_read, skip, callback) {
         var filter_options = {
             limit: ticks_to_read,
@@ -32,25 +39,25 @@ var ticks_dal = {
             skip: skip
         };
         var filter = {
-            timestamp: {'$lt': timestamp}
+            timestamp: { '$lt': timestamp }
         };
-        ticks_collection.find(filter, filter_options, function (err, docs) {
+        db.collection('ticks').find(filter, filter_options, function (err, docs) {
             if (err) {
                 logger.error('Cannot read ticks. err: %s', err);
             }
             if (callback) callback(docs);
         });
     },
-    
-    get_last_valid_stat: function (stat_id, callback){
+
+    get_last_valid_stat: function (stat_id, callback) {
         var filter = [
             { '$sort': { 'timestamp': -1 } },
             { '$unwind': '$stats' },
             { '$match': { 'stats.id': stat_id, 'stats.value': { '$exists': true } } },
             { '$project': { 'name': '$stats.name', 'value': '$stats.value', 'variable': '$stats.variable' } },
-            { '$limit': 1}
+            { '$limit': 1 }
         ];
-        ticks_collection.aggregation(filter, {}, function (err, docs) {
+        db.collection('ticks').aggregation(filter, {}, function (err, docs) {
             if (err) {
                 logger.error('Cannot read stat. err: %s', err);
             }
@@ -58,36 +65,36 @@ var ticks_dal = {
         });
     }
 };
-
-var stats_collection = db.get('stats');
-var stats_dal = {
-    insert: function (stat, callback) {
-        stat.timestamp = Date.now();
-        //make sure _id is unique
-        stat._id = new ObjectID();
-        stats_collection.insert(stat, function (err, doc) {
-            if (err) {
-                logger.error('Cannot store current stat. err: %s stat: ', err, stat);
-            }
-            if (callback) callback(doc);
-        });
-    },
-
-    read_last_stats: function (stats_to_read, callback) {
-        var filter_options = {
-            limit: stats_to_read,
-            sort: [['timestamp', 'desc']]
-        };
-        ticks_collection.find({}, filter_options, function (err, docs) {
-            if (err) {
-                logger.error('Cannot read ticks. err: %s', err);
-            }
-            if (callback) callback(docs);
-        });
-    }
-};
-
-module.exports = {
-    ticks_dal: ticks_dal,
-    stats_dal: stats_dal
-};
+// 
+// var stats_collection = db.get('stats');
+// var stats_dal = {
+//     insert: function (stat, callback) {
+//         stat.timestamp = Date.now();
+//         //make sure _id is unique
+//         stat._id = new ObjectID();
+//         stats_collection.insert(stat, function (err, doc) {
+//             if (err) {
+//                 logger.error('Cannot store current stat. err: %s stat: ', err, stat);
+//             }
+//             if (callback) callback(doc);
+//         });
+//     },
+// 
+//     read_last_stats: function (stats_to_read, callback) {
+//         var filter_options = {
+//             limit: stats_to_read,
+//             sort: [['timestamp', 'desc']]
+//         };
+//         ticks_collection.find({}, filter_options, function (err, docs) {
+//             if (err) {
+//                 logger.error('Cannot read ticks. err: %s', err);
+//             }
+//             if (callback) callback(docs);
+//         });
+//     }
+// };
+// 
+// module.exports = {
+//     ticks_dal: ticks_dal,
+//     stats_dal: stats_dal
+// };
